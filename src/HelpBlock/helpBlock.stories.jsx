a976a9d56ffed3e5bb857f6b6b37c0007eaabd2f
Svitlana Liulka <svitlana.liulka@paysera.net>
2023-01-10 12:25:08 +0200

    SUPPORT-65374 Create HelpBlock module of storybook

---
+++ src/HelpBlock/helpBlock.stories.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Form, FormGroup, HelpBlock,
+} from '@paysera/react-common';
+import { PAYSERA_COMPONENTS } from '../const';
+import withReduxForm from '../utils/withReduxForm';
+
+export default {
+    title: `${PAYSERA_COMPONENTS}/HelpBlock`,
+    component: Form,
+    decorators: [
+        withReduxForm,
+    ],
+};
+
+const Template = args => (
+    <Form {...args}>
+        <FormGroup placeholder="Test" />
+        <HelpBlock>I`m helper block</HelpBlock>
+    </Form>
+);
+
+export const HelpBlockComponent = Template.bind({});
+
+HelpBlockComponent.args = {
+};
