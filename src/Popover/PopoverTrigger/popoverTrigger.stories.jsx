d248177223bab3ceef048538d514373cc0ff37f2
Svitlana Liulka <svitlana.liulka@paysera.net>
2023-01-20 19:22:44 +0200

    SUPPORT-65368 - Create popover module in storybook

--- src/Popover/PopoverTrigger/popoverTrigger.stories.jsx
+++ src/Popover/PopoverTrigger/popoverTrigger.stories.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import {
-    Button, ButtonToolbar, OverlayTrigger as OverlayTriggerComponent, Popover as PopoverComponent,
+    Button, ButtonToolbar, OverlayTrigger as OverlayTriggerComponent, Popover,
 } from '@paysera/react-common';
-import { Popover } from '../popover.stories';
 
 export default {
     title: 'Popover Components/OverlayTrigger',
-    component: PopoverComponent,
+    component: Popover,
 };
 
 const Template = args => (
     <ButtonToolbar {...args}>
-        <OverlayTriggerComponent trigger="click" placement="bottom" overlay={<Popover />}>
-            <Button>Click Popover</Button>
-        </OverlayTriggerComponent>
-        <OverlayTriggerComponent trigger="focus" placement="top" overlay={<Popover />}>
+        <OverlayTriggerComponent
+            trigger="focus"
+            placement="bottom"
+            overlay={(<Popover>Holy guacamole!</Popover>)}
+        >
             <Button>Focus Popover</Button>
         </OverlayTriggerComponent>
-        <OverlayTriggerComponent trigger="hover" placement="right" overlay={<Popover />}>
+        <OverlayTriggerComponent
+            trigger="hover"
+            placement="bottom"
+            overlay={(<Popover>Holy guacamole!</Popover>)}
+        >
             <Button>Hover Popover</Button>
+        </OverlayTriggerComponent>
+        <OverlayTriggerComponent
+            trigger="click"
+            placement="right"
+            overlay={(<Popover>Holy guacamole!</Popover>)}
+        >
+            <Button>Click Popover</Button>
         </OverlayTriggerComponent>
     </ButtonToolbar>
 );
